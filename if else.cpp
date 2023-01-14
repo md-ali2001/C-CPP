868be4279bc2444a74f3d26c5f62d2ff598c7a0b
Muhammd Ali Masood <md.ali2001@hotmail.com>
2023-01-15 01:07:49 +0500

    Create if else.cpp

---
+++ if else.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    int a=12;
+  
+    
+    // if(a==2)
+    // cout<<"the value is 2";
+    
+    // else if(a>=2)
+    // cout<<"the value is greater than or equal to 2";
+    
+    // else
+    // cout<<"the value is lesser than 2";
+    
+    // if(a<1)
+    // cout<<"the value is lesser than 1"<<endl;
+    
+    //  if(a==1)
+    // cout<<"the value is equal to 1"<<endl;
+    
+    //  if(a>1)
+    // cout<<"the value is greater than 1"<<endl;
+    
+    //  if(a>5)
+    // cout<<"the value is greater than 5"<<endl;
+    
+    // if(a<15)
+    // cout<<"the value is lesser than 15";
+    
+    // if(a==4)
+    // {
+    // cout<<"the value is 4\n";
+    //  if(a<4)
+    // cout<<"the value is greater than 2";
+    
+    // else if (a>6)
+    // cout<<"the value is greater than 6";
+    
+    // }
+    
+    // else if(a>8)
+    // {
+    
+    //     cout<<"the value is greater than 8";
+    //       if(a>10)
+    //     cout<<"the value is greater than 10";
+        
+    //     else if (a<10)
+    //     cout<<"the value is lesser than 10";
+        
+        
+    // }
+    
+    // else
+    // cout<<"the value is lesser than 8";
+    
+    
+    
+    //3 types of loops   
+    
+    /*
+    for loop , while loop , do while loop
+    
+    for loop--> number of iterations known
+    while loop--> exact number of iterations unknown--> first , condition is checked, then statement is executed
+    while-->for loop
+    do while ()--> statement is executed first then condition is checked
+    
+    
+    
+    */
+    
+    if(a==6)
+    cout<<"the value is 6"<<endl;
+    
+    else if(a>6){
+    cout<<"the value is greater than 6"<<endl;
+    
+    if(a>8)
+    cout<<"the value is greater than 8"<<endl;
+    else if (a>10)
+    cout<<"the value is 10"<<endl;
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+
+    return 0;
+}
