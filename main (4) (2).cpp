e12d2ce32b651ed128fdcc6849fabd14ddf86698
Muhammd Ali Masood <md.ali2001@hotmail.com>
2023-01-15 00:58:57 +0500

    Add files via upload

---
+++ main (4) (2).cpp
@@ -0,0 +1,120 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int
+main ()
+{
+
+
+  /*
+     do
+     {
+     body
+     }
+     while();
+
+   */
+
+
+
+  /* 
+     initializer
+     while()
+     {
+     variable update  
+     }
+   */
+
+
+  // int i=1;
+  // do 
+  // {
+  //   cout<<"hello\n"  ; 
+  //       i++;                    //cout<<"hello"<<endl;      
+  //                               // semi colon after while in do while loop but not in while loop
+  // }
+
+  // while(i<1);
+
+
+
+  // int i=1;                   // do while loop is used when we must have to execute body atleast once
+  // while(i<1)
+  // {                         // there is a possibility that while loop body not executes at all
+  //     cout<<"hello\n";
+  //     i++;
+  // }
+
+
+
+  // char ch='p';
+
+  // do
+  // {
+  //     cout<<"enter again\n";
+  //     cin>>ch;
+  // }
+  // while(ch!='p');
+
+
+
+  /*
+
+     do{
+
+
+
+     }
+     while();                                     for(){}         while(){}
+
+
+
+   */
+
+//   int i = 0, j = 0;
+
+//   do
+//     {
+//       do
+// 	{
+// 	  cout << "h\n";
+// 	  j++;
+// 	}
+
+//       i++;
+//       cout << "h\n";
+//       }
+
+//       while (i < 4);
+//       while (j < 4);
+      
+      
+      
+      
+      
+      
+      //********functions in c/c++************
+/* larger problem can be broken into smaller pieces
+   each part of program can be tested individually
+   reusabality
+   
+   function has :
+   prototype-->function is declared but not defined--> function return datatype --> function args/parameters/inputs
+   body/definition--> internal processing of function/implementation 
+   it is not necessary that function returns value , it is not necessary that function takes args
+*/
+
+
+
+
+      return 0;
+      }
