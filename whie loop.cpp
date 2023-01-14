c6096be94adcf017cbdfec92dc61bec60e6c066f
Muhammd Ali Masood <md.ali2001@hotmail.com>
2023-01-15 01:03:45 +0500

    Create whie loop.cpp

---
+++ whie loop.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char ch;
+    
+    /*
+    while(condition)
+    {
+        body / statements to be executed
+    }
+    
+    
+    
+    */
+    
+    // while(ch!='p') 
+    // {
+    //     cout<<"enter character again\n";
+    //     cin>>ch;
+    //     ch=tolower(ch);
+    // for()
+    // }
+    
+    
+    /*
+    initializer  
+    for(;condition;)
+    {
+        variable update
+    }
+    */
+    
+    
+    /*
+    number of iterations known-->good practice : for loop use krlo : but while loop bh lgskta
+    number of iterations unknown--> while loop is must : for loop cant : do while loop also
+    
+    
+    */
+    
+    // int i=0;
+    // while(i<5)
+    // {
+    //     cout<<"Abdullah\n";
+    //     i++;
+    // }
+    
+    
+    // int i=1;     // outer loop    1 se 5 tk ka table print krwana he
+    // int j=1;    // inner loop     1 ka table 10 tk print krwana he
+    // while(i<6 && j<11)
+    // {
+    //     cout<<i<<" * "<<j<<" = "<<i*j<<endl; // 1 x1   1x2=2   1x3
+    //     j++;
+        
+    //     if(j==11)
+    //     {
+    //         cout<<endl;  // for new line after completing one table
+    //         i++;               // i=2   j=1          //i=3   j=1
+    //         j=1;
+            
+    //     }
+        
+        
+    // }
+    
+    
+    
+    
+    
+    
+    
+    // int i=0;
+    // int j=0;
+    
+    // while(i<6)
+    // {
+        
+      
+        
+    //     while(j<7)
+    //     {
+    //     cout<<"j = "<<j<<"\t";   // 0 1 2 3 4 5 6    
+    //     j++;                     // 0 1 2 3 4 5 6 
+    //                              // 0 1 2 3 4 5 6
+    //     }  
+        
+        
+           
+    //     j=0;
+    //       i++;
+    //       cout<<endl;             // 0 1 2 3 4 5 6
+    //                              // 0 1 2 3 4 5 6
+    //                              // 0 1 2 3 4 5 6
+       
+        
+    // }
+    
+    
+    
+    
+
+    return 0;
+}
